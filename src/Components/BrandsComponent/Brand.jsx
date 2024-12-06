12ec890507fe3759e6fefbe2c5cf0c9d926459ec
gokulvasanthJ <gokul27mech@gmail.com>
2024-12-07 02:56:05 +0530

    adding responsiveness

--- src/Components/BrandsComponent/Brand.jsx
+++ src/Components/BrandsComponent/Brand.jsx
@@ -1,58 +1,56 @@
-import React from 'react'
-import brand1 from '../BrandsComponent/brandimg/28.png'
-import brand2 from '../BrandsComponent/brandimg/27.png'
-import brand3 from '../BrandsComponent/brandimg/29.png'
-import brand4 from '../BrandsComponent/brandimg/30.png'
-import brand5 from '../BrandsComponent/brandimg/32.png'
+import React from "react";
+import brand1 from "../BrandsComponent/brandimg/28.png";
+import brand2 from "../BrandsComponent/brandimg/27.png";
+import brand3 from "../BrandsComponent/brandimg/29.png";
+import brand4 from "../BrandsComponent/brandimg/30.png";
+import brand5 from "../BrandsComponent/brandimg/32.png";
 
-import '../BrandsComponent/Brand.css'
-
-
-
+import "../BrandsComponent/Brand.css";
 
 function Brand() {
-  return<>
-  <div className="brand-container">
+  return (
+    <>
+      <div className="brand-container">
+        <h6>
+          “Practical Pieces Made With Thoughtful,
+          <br />
+          Design Treatments”
+        </h6>
 
-  <h6>“Practical Pieces Made With Thoughtful,<br/>
-  Design Treatments”</h6>
+        <div class="row row-cols-1 row-cols-md-5 g-3">
+          <div class="col">
+            <div class="card h-100 brand-img">
+              <img src={brand1} class="card-img-top" alt="..." />
+            </div>
+          </div>
 
-  <div class="row row-cols-1 row-cols-md-5 g-3">
-    
-  <div class="col">
-  <div class="card h-100 brand-img">
-  <img src={brand1} class="card-img-top" alt="..."/>
-  </div>
-  </div> 
+          <div class="col">
+            <div class="card h-100 brand-img">
+              <img src={brand2} class="card-img-top" alt="..." />
+            </div>
+          </div>
 
-  <div class="col">
-  <div class="card h-100 brand-img">
-  <img src={brand2} class="card-img-top" alt="..."/>
-  </div>
-  </div> 
+          <div class="col">
+            <div class="card h-100 brand-img">
+              <img src={brand3} class="card-img-top" alt="..." />
+            </div>
+          </div>
 
-  <div class="col">
-  <div class="card h-100 brand-img">
-  <img src={brand3} class="card-img-top" alt="..."/>
-  </div>
-  </div> 
+          <div class="col">
+            <div class="card h-100 brand-img">
+              <img src={brand4} class="card-img-top" alt="..." />
+            </div>
+          </div>
 
-  <div class="col">
-  <div class="card h-100 brand-img">
-  <img src={brand4} class="card-img-top" alt="..."/>
-  </div>
-  </div> 
-
-  <div class="col">
-  <div class="card h-100 brand-img">
-  <img src={brand5} class="card-img-top" alt="..."/>
-  </div>
-  </div> 
-
-</div>
-  </div>
-
-  </>
+          <div class="col">
+            <div class="card h-100 brand-img">
+              <img src={brand5} class="card-img-top" alt="..." />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Brand
+export default Brand;
--- src/Pages/HomePages.jsx
+++ src/Pages/HomePages.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import Navbars from '../Components/NavbarComponent/Navbars'
-import Crousels from '../Components/CrouselComponent/Crousels'
-import Catogories from '../Components/CatogoriesComponent/Catogories'
-import Banner from '../Components/BannerCtgrCmpnt/Banner'
-import TopProducts from '../Components/TopProductsCmpnt/TopProducts'
-import ProductPackage from '../Components/ProductPackage/ProductPackage'
-import Footer from '../Components/footer/Footer'
-import Blog from '../Components/BlogComponent/Blog'
-import Brand from '../Components/BrandsComponent/Brand'
-
+import React from "react";
+import Navbars from "../Components/NavbarComponent/Navbars";
+import Crousels from "../Components/CrouselComponent/Crousels";
+import Catogories from "../Components/CatogoriesComponent/Catogories";
+import Banner from "../Components/BannerCtgrCmpnt/Banner";
+import TopProducts from "../Components/TopProductsCmpnt/TopProducts";
+import ProductPackage from "../Components/ProductPackage/ProductPackage";
+import Footer from "../Components/footer/Footer";
+import Blog from "../Components/BlogComponent/Blog";
+import Brand from "../Components/BrandsComponent/Brand";
 
 function HomePages() {
-  return<>
-  
-  <Navbars/>
-  <Crousels/>
-  <Catogories/>
-  <Banner/>
-  <TopProducts/>
-  <ProductPackage/>
-  <Blog/>
-  <Brand/>
-  <Footer/>
-
-  </>
+  return (
+    <>
+      <Navbars />
+      <Crousels />
+      <Catogories />
+      <Banner />
+      <TopProducts />
+      <ProductPackage />
+      <Blog />
+      <Brand />
+      <Footer />
+    </>
+  );
 }
 
-export default HomePages
+export default HomePages;
